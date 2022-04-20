db9f5adc372129ee94c83c77bcb66f3f7c0c7bca
Dinghao Liu <31622029+dinghaoliu@users.noreply.github.com>
2022-04-20 15:13:13 +0800

    Update llvm

---
+++ llvm/build-llvm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# binary choices
+BASE="http://releases.llvm.org"
+VERN="9.0.0"
+
+# platform specifics
+case "$OSTYPE" in
+  linux*)
+    DIST="x86_64-linux-gnu-ubuntu-16.04"
+    ;;
+  darwin*)
+    DIST="x86_64-darwin-apple"
+    ;;
+  bsd*)
+    DIST="amd64-unknown-freebsd10"
+    ;;
+  *)
+    echo "Unknown OS ($OSTYPE)"; exit 
+    ;;
+esac
+
+# download pre-built package 
+PKGN=clang+llvm-$VERN-$DIST
+TARF=$PKGN.tar.xz
+
+LINK=$BASE/$VERN/$TARF
+wget $LINK
+
+# extract to location
+tar xvf $TARF
+mv $PKGN build
